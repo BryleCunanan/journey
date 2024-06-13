bc8215f7764901fc4bd50efc4b36be300bb8b849
Bryle Paulo Cunanan <brylepaulocunanan@gmail.com>
2024-06-13 14:25:14 +0800

    base

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import ImageViewer from './components/ImageViewer';
+
+const PlaceholderImage = require('./assets/favicon.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style ={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage}/>
+      </View>
+      <StatusBar style='auto'/>
     </View>
   );
 }
@@ -13,8 +18,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#35374B',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
   },
 });
