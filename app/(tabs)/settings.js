08a2745a7c4feafbe2b493d320ccc87755b4db9a
Alessandra Lazarte <lazartealexs@gmail.com>
2024-06-30 22:19:26 +0800

    added 'appearance', 'themes', and color balls

--- app/(tabs)/settings.js
+++ app/(tabs)/settings.js
@@ -1,10 +1,32 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import React, { useContext } from "react";
 import { ThemeContext } from "../../helpers/ThemeContext";
 import ThemeSwitcher from "../../components/ThemeSwitcher";
 
 const ExampleComponent = () => {
   const { theme } = useContext(ThemeContext);
+
+  const themeColor = [
+    { id: "red", backgroundColor: "#ff6c6c" },
+    { id: "pink", backgroundColor: "#ff66c4" },
+    { id: "violet", backgroundColor: "#8c52ff" },
+    { id: "blue", backgroundColor: "#0cc0df" },
+    { id: "yellow", backgroundColor: "#ffbd59" },
+    { id: "black", backgroundColor: "#252525" },
+  ];
+
+  const BgColor = ({ backgroundColor }) => (
+    <View
+      style={{
+        height: 32,
+        width: 32,
+        borderRadius: 50,
+        borderWidth: 1,
+        borderColor: "#b28771",
+        backgroundColor: backgroundColor,
+      }}
+    ></View>
+  );
 
   return (
     <View
@@ -13,13 +35,38 @@ const ExampleComponent = () => {
         alignItems: "center",
         // backgroundColor: theme.backgroundColor,
         backgroundColor: "#F8EBDE",
-        padding: 10,
+        padding: 30,
+        gap: 40,
       }}
     >
       <View
         style={{
           backgroundColor: "#ffffff",
-          width: "80%",
+          width: "100%",
+          height: "40%",
+          borderRadius: 25,
+          elevation: 8,
+        }}
+      >
+        <Text>Appearance</Text>
+        <Text>Themes</Text>
+        <View style={{ alignItems: "center" }}>
+          <FlatList
+            data={themeColor}
+            renderItem={({ item }) => (
+              <BgColor backgroundColor={item.backgroundColor} />
+            )}
+            keyExtractor={(item) => item.id}
+            horizontal
+            ItemSeparatorComponent={() => <View style={{ width: 20 }} />}
+          />
+        </View>
+      </View>
+
+      <View
+        style={{
+          backgroundColor: "#ffffff",
+          width: "100%",
           height: "40%",
           borderRadius: 25,
           elevation: 5,
