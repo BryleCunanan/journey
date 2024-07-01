55532069ed6fc693e2b310144f3a7fd18559327f
Alessandra Lazarte <lazartealexs@gmail.com>
2024-07-01 12:34:30 +0800

    Added pressable and flatlist for font style

--- app/(tabs)/settings.js
+++ app/(tabs)/settings.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList, Pressable } from "react-native";
 import React, { useContext } from "react";
 import { ThemeContext } from "../../helpers/ThemeContext";
 import ThemeSwitcher from "../../components/ThemeSwitcher";
@@ -12,7 +12,6 @@ const ExampleComponent = () => {
     { id: "violet", backgroundColor: "#8c52ff" },
     { id: "blue", backgroundColor: "#0cc0df" },
     { id: "yellow", backgroundColor: "#ffbd59" },
-    { id: "black", backgroundColor: "#252525" },
   ];
 
   const BgColor = ({ backgroundColor }) => (
@@ -27,8 +26,19 @@ const ExampleComponent = () => {
       }}
     ></View>
   );
+  const fontDropdown = [
+    { id: "poppins", fontFamily: "Poppins" },
+    { id: "inter", fontFamily: "Inter" },
+    { id: "satoshi", fontFamily: "Satoshi" },
+    { id: "open sans", fontFamily: "Open Sans" },
+    { id: "Roboto", fontFamily: "Roboto" },
+  ];
+  const FontFamilyDropdown = ({ fontFamily }) => (
+    <View style={{ width: 200, backgroundColor: "#ffffff" }}></View>
+  );
 
   return (
+    //main page
     <View
       style={{
         flex: 1,
@@ -39,13 +49,14 @@ const ExampleComponent = () => {
         gap: 40,
       }}
     >
-      <View
+      <View //Container for appearance
         style={{
           backgroundColor: "#ffffff",
           width: "100%",
           height: "40%",
           borderRadius: 25,
           elevation: 8,
+          padding: 20,
         }}
       >
         <Text>Appearance</Text>
@@ -61,9 +72,34 @@ const ExampleComponent = () => {
             ItemSeparatorComponent={() => <View style={{ width: 20 }} />}
           />
         </View>
+        <View style={{ flexDirection: "row" }}>
+          <Text>Font Style</Text>
+          <View>
+            <Pressable
+              onPress={() => {
+                console.log("Clicked") ;
+              }}
+              style={{
+                borderWidth: 2,
+                width: 200,
+                height: 30,
+                borderRadius: 25,
+              }}
+            ></Pressable>
+            <FlatList
+              data={fontDropdown}
+              renderItem={({ item }) => (
+                <FontFamilyDropdown fontFamily={item.fontFamily} />
+              )}
+              keyExtractor={(item) => item.id}
+            />
+          </View>
+        </View>
+
+        <Text>Font Size</Text>
       </View>
 
-      <View
+      <View //Container for notification
         style={{
           backgroundColor: "#ffffff",
           width: "100%",
