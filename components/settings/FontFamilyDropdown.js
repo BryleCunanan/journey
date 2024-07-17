fe90b266f74759a666dc6041e8da8b2b273f3177
Bryle Cunanan <brylepaulocunanan@gmail.com>
2024-07-17 12:09:45 +0800

    changed dropdown to a modal

--- components/settings/FontFamilyDropdown.js
+++ components/settings/FontFamilyDropdown.js
@@ -1,24 +1,39 @@
 import React, { useContext, useState } from "react";
 import { ThemeContext } from "../../helpers/ThemeContext";
-import { FlatList, Pressable, View, Text } from "react-native";
+import { FlatList, Pressable, View, Text, Modal } from "react-native";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { BlurView } from "expo-blur";
 
 const FontFamilyDropdown = ({ fontList, isDropDown, setIsDropDown }) => {
-  const { theme } = useContext(ThemeContext);
+  const { theme, font, changeFont } = useContext(ThemeContext);
 
   const handleDropdown = () => {
     setIsDropDown(!isDropDown);
   };
 
   const Dropdown = ({ fontFamily }) => (
-    <View
+    <Pressable
       style={{
         width: "100%",
-        paddingVertical: 8,
+        padding: 8,
+        backgroundColor:
+          fontFamily == font ? theme.primaryColor : theme.secondaryColor,
+        borderRadius: 12,
       }}
+      onPress={() => {
+        changeFont(fontFamily);
+      }}
+      hitSlop={{ top: 15, bottom: 15, left: 30, right: 30 }}
     >
-      <Text style={{ color: theme.primaryColor }}>{fontFamily}</Text>
-    </View>
+      <Text
+        style={{
+          color: fontFamily == font ? theme.secondaryColor : theme.primaryColor,
+          fontFamily: fontFamily,
+        }}
+      >
+        {fontFamily}
+      </Text>
+    </Pressable>
   );
 
   return (
@@ -43,48 +58,93 @@ const FontFamilyDropdown = ({ fontList, isDropDown, setIsDropDown }) => {
             justifyContent: "space-between",
           }}
         >
-          <Text style={{ color: theme.primaryColor, fontSize: 16 }}>Hello</Text>
-          {isDropDown ? (
-            <FontAwesome name="caret-up" color={theme.primaryColor} size={20} />
-          ) : (
-            <FontAwesome
-              name="caret-down"
-              color={theme.primaryColor}
-              size={20}
-            />
-          )}
+          <Text
+            style={{
+              color: theme.primaryColor,
+              fontSize: 16,
+              fontFamily: font,
+            }}
+          >
+            {font}
+          </Text>
         </View>
       </Pressable>
-      {isDropDown ? (
-        <FlatList
-          style={{
-            backgroundColor: theme.secondaryColor,
-            padding: 10,
-            paddingTop: 15,
-            width: "100%",
-            alignSelf: "center",
-            borderBottomLeftRadius: 12,
-            borderBottomRightRadius: 12,
-            position: "absolute",
-            marginTop: 24,
-            zIndex: 2,
-            borderWidth: 2,
-            borderColor: theme.primaryColor,
-          }}
-          data={fontList}
-          renderItem={({ item }) => <Dropdown fontFamily={item.fontFamily} />}
-          keyExtractor={(item) => item.id}
-          ItemSeparatorComponent={() => (
-            <View
+      <Modal
+        transparent={true}
+        visible={isDropDown}
+        statusBarTranslucent={true}
+        animationType="fade"
+      >
+        <BlurView
+          intensity={40}
+          experimentalBlurMethod="dimezisBlurView"
+          blurReductionFactor={6}
+          //   tint="dark"
+        >
+          <Pressable
+            style={{
+              width: "100%",
+              height: "100%",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            onPress={() => {
+              setIsDropDown(false);
+            }}
+          >
+            <Pressable
               style={{
-                width: "100%",
-                backgroundColor: theme.primaryColor,
-                height: 2,
+                width: "60%",
+                height: "30%",
+                backgroundColor: theme.secondaryColor,
+                elevation: 5,
+                borderRadius: 14,
+                borderWidth: 2,
+                borderColor: theme.primaryColor,
               }}
-            />
-          )}
-        />
-      ) : null}
+              onPress={() => {}}
+            >
+              <FlatList
+                style={{
+                  padding: 10,
+                  width: "100%",
+                  alignSelf: "center",
+                  zIndex: 2,
+                }}
+                data={fontList}
+                renderItem={({ item }) => (
+                  <Dropdown fontFamily={item.fontFamily} />
+                )}
+                keyExtractor={(item) => item.id}
+                ItemSeparatorComponent={() => (
+                  <View
+                    style={{
+                      width: "100%",
+                      backgroundColor: theme.primaryColor,
+                      height: 2,
+                      marginVertical: 4,
+                    }}
+                  />
+                )}
+              />
+              <Pressable
+                style={{
+                  width: 32,
+                  backgroundColor: theme.primaryColor,
+                  height: 24,
+                  borderRadius: 4,
+                }}
+                onPress={() => {
+                  setIsDropDown(false);
+                }}
+                hitSlop={{ top: 30, bottom: 30, left: 30, right: 30 }}
+              >
+                <Text>Back</Text>
+              </Pressable>
+            </Pressable>
+          </Pressable>
+        </BlurView>
+      </Modal>
     </View>
   );
 };
