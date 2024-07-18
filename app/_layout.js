22486836b16075625db11f7a1560e1930f25d8d6
Bryle Cunanan <brylepaulocunanan@gmail.com>
2024-07-19 05:03:24 +0800

    local authentication

--- app/_layout.js
+++ app/_layout.js
@@ -1,14 +1,70 @@
 import { Stack } from "expo-router/stack";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ThemeContext, ThemeProvider } from "../helpers/ThemeContext";
+import * as LocalAuthentication from "expo-local-authentication";
+import {
+  Alert,
+  BackHandler,
+  View,
+  Text,
+  ActivityIndicator,
+} from "react-native";
 
-export default function Layout() {
+const Layout = () => {
+  const [authSuccess, setAuthSuccess] = useState(false); // Track authentication status
+
+  const authenticate = async () => {
+    try {
+      const hasHardware = await LocalAuthentication.hasHardwareAsync();
+      if (!hasHardware) {
+        Alert.alert("Biometric hardware not available");
+        return;
+      }
+
+      const isEnrolled = await LocalAuthentication.isEnrolledAsync();
+      if (!isEnrolled) {
+        Alert.alert("No biometrics enrolled");
+        return;
+      }
+
+      const result = await LocalAuthentication.authenticateAsync({
+        promptMessage: "Authenticate",
+        fallbackLabel: "Enter passcode",
+        cancelLabel: "Cancel",
+      });
+
+      if (result.success) {
+        setAuthSuccess(true); // Authentication successful
+      } else {
+        Alert.alert("Authentication failed", "Would you like to retry?", [
+          { text: "Retry", onPress: () => authenticate() },
+          { text: "Exit", onPress: () => BackHandler.exitApp() },
+        ]);
+      }
+    } catch (error) {
+      Alert.alert("An error occurred", error.message);
+    }
+  };
+
+  useEffect(() => {
+    authenticate();
+  }, []);
+
   return (
     <ThemeProvider>
-      <ThemedStack />
+      {authSuccess ? (
+        <ThemedStack />
+      ) : (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <ActivityIndicator size="large" color="#0000ff" />
+          <Text>Authenticating...</Text>
+        </View>
+      )}
     </ThemeProvider>
   );
-}
+};
 
 const ThemedStack = () => {
   const { theme, font, fontSize } = useContext(ThemeContext);
@@ -36,3 +92,5 @@ const ThemedStack = () => {
     </Stack>
   );
 };
+
+export default Layout;
